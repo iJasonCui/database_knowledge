48005f8ebf8fe39ac7b3f71c4aa059214f50920b
jason cui <jasoncui@jasons-MacBook-Air.local>
2014-10-21 23:51:13 -0400

    bug fix

--- Sybase/SybaseASE/sproc/RevenueCanada/t1_2014.sh
+++ Sybase/SybaseASE/sproc/RevenueCanada/t1_2014.sh
@@ -49,9 +49,9 @@ line234=`echo $line150 - $line233  | bc`
 line236=$line234
 line260=$line236
 
-echo "Line 101: "$T4Box14_line101       > ${OUTPUT_FILE}
-echo "Line 117: "$UCCB_line117          >> ${OUTPUT_FILE}
-echo "Line 121: "$INTREST_line121       >> ${OUTPUT_FILE}
+echo "Line 101: "$T4Box14_line101       		> ${OUTPUT_FILE}
+echo "Line 117: "$UCCB_line117          		>> ${OUTPUT_FILE}
+echo "Line 121: "$INTREST_line121       		>> ${OUTPUT_FILE}
 echo "Line 150: "$line150                       >> ${OUTPUT_FILE}
 echo "Line 206: "$PensionAdjustBox52            >> ${OUTPUT_FILE}
 echo "Line 208: "$RRSP_line208                  >> ${OUTPUT_FILE}
@@ -63,26 +63,144 @@ echo "Line 260: "$line260                       >> ${OUTPUT_FILE}
 
 
 ##schedule 1
-echo "#----------------------------------------------------"
-echo "Go to Schedule 1"
-EXEC tsp_getSchedule1_2012 $line260, $spouseNetIncome, $combineFlag, $CPP, $EI, $Donation, $childrenAmount, $line420 output, $col345 output, $col347 ou
-tput
-echo "Line 420: "$line420
-echo "#----------------------------------------------------"
-echo "Go to Ontario Tax 428"
+
+echo "#----------------------------------------" >> ${OUTPUT_FILE}
+echo "Go to Schedule 1" >> ${OUTPUT_FILE}
+
+col300= 10822
+col308= $CPP ##MAX 2217.60
+col312= $EI ##MAX 786.76
+##col367= 2 * 2191
+col367= $childrenAmount
+col363= 1095 ##canada employment
+col364= 1285 ##10 months
+col365=`echo  2 * 500 | bc` ## fitness amount
+col370=`echo  2 * 500 | bc` ##art amount
+##col368= 10000 ## home renovation expense schedule 12 only for 2009
+col315=`echo  2 * 4402 | bc`  ## caregiver amount schedule 5 changjiu only for now; kairong tax year 2011
+
+if [ "$combineFlag" -eq 1 ] ## CONBINED
+then
+    col303 =`echo  $col300 - $spouseNetIncome | bc `
+
+    if [ "$col303" -lt 0 ]
+    then
+        col303 = 0
+    fi
+else
+    col303 = 0
+fi
+
+##calculate the Line18
+col335 =`echo  $col300 + $col303 + $col367 + $col308 + $col312 + $col363 + $col364 + $col365 + $col315 + $col370 | bc` 
+col338 =`echo  $col335 * 0.15 | bc`
+
+##donation
+col340 = $Donation
+
+if [ "$col340" -ge 200 ]
+then
+    col345 = 200
+else
+    col345 = $col340
+fi
+
+col346 =`echo  $col345 *0.15     | bc`
+col347 =`echo  $col340 - $col345 | bc`
+col348 =`echo  $col347 *0.29     | bc`
+col349 =`echo  $col346 + $col348 | bc`
+col350 =`echo  $col338 + $col349 | bc`
+
+##DECLARE $LINE36 numeric(12,2)
+if [ "$col260" -le 42707 ] 
+then
+    LINE39 = `echo $col260 * 0.15 | bc `
+fi
+
+if [ "$col260" -gt 42707 ] && [ "$col260" -le 85414 ] 
+then
+    LINE39 = `echo 6406 + ($col260 - 42707) * 0.22 | bc `
+fi
+
+if [ "$col260" -gt 85414 ] && [ "$col260" -le 132406 ]
+then
+    LINE32 = $col260
+fi
+
+LINE35 = `echo $col260 - 85414 | bc `
+LINE37 = `echo ($col260 - 85414) * 0.26) | bc `
+LINE39 = `echo 15802 + $LINE37 | bc `
+
+if [ "$col260" -gt  127021 ] 
+then
+    LINE39 = `echo 28020 + ($col260 - 132406) * 0.29 | bc `
+
+    ## calculate the Line 38
+    col420 = $LINE39 - $col350
+    
+    if [ "$col420" -lt 0 ] 
+    then 
+        col420 = 0
+    fi
+fi
+
+echo "Line 303: "$col303 >> ${OUTPUT_FILE}
+echo "Line 367: "$col367 >> ${OUTPUT_FILE}
+echo "Line 308: "$col308 >> ${OUTPUT_FILE}
+echo "Line 312: "$col312 >> ${OUTPUT_FILE}
+echo "Line 363: "$col363 >> ${OUTPUT_FILE}
+echo "Line 364: "$col364 >> ${OUTPUT_FILE}
+echo "Line 365: "$col365 >> ${OUTPUT_FILE}
+echo "Line 370: "$col370 >> ${OUTPUT_FILE}
+echo "Line 315: "$col315 >> ${OUTPUT_FILE}
+echo "Line 335: "$col335 >> ${OUTPUT_FILE}
+##LINE 29 
+echo "Line 338: "$col338 >> ${OUTPUT_FILE}
+echo "Line 340: "$col340 >> ${OUTPUT_FILE}
+echo "Line 345: "$col345 >> ${OUTPUT_FILE}
+echo "Line 346: "$col346 >> ${OUTPUT_FILE}
+echo "Line 347: "$col347 >> ${OUTPUT_FILE}
+echo "Line 348: "$col348 >> ${OUTPUT_FILE}
+echo "Line 349: "$col349 >> ${OUTPUT_FILE}
+echo "Line 350: "$col350 >> ${OUTPUT_FILE}
+echo "Line 32 : "$LINE32 >> ${OUTPUT_FILE}
+echo "Line 33 : "$LINE32 >> ${OUTPUT_FILE}
+echo "Line 35 : "$LINE35 >> ${OUTPUT_FILE}
+echo "Line 37 : "$LINE37 >> ${OUTPUT_FILE}
+echo "Line 39 : "$LINE39 >> ${OUTPUT_FILE}
+echo "Line 40 : "$LINE39 >> ${OUTPUT_FILE}
+echo "Line 42 : "$LINE39 >> ${OUTPUT_FILE}
+echo "Line 43 : "$col350 >> ${OUTPUT_FILE}
+echo "Line 420: "$col420 >> ${OUTPUT_FILE}
+echo "#----------------------------------------" >> ${OUTPUT_FILE}
+
+echo "Go to Ontario Tax 428" >> ${OUTPUT_FILE}
+
 ##ontario tax 428
-EXEC tsp_getON428_2012 $line260, $spouseNetIncome, $combineFlag , $CPP, $EI, $col345, $col347, $line428 output
+
+##EXEC tsp_getON428_2012 $line260, $spouseNetIncome, $combineFlag , $CPP, $EI, $col345, $col347, $line428 output
 echo "Line 428: "$line428
 echo "The end of Ontario Tax 428"
-echo "#----------------------------------------------------"
+echo "#----------------------------------------------------" >> ${OUTPUT_FILE}
+
+
 $line435 = $line420 + $line428
 echo "Line 435: "$line435
 echo "Line 437: "$TAX_DEDUCTED_line437
-$line479 = 105.2
+line479 = 105.2
 echo "Line 479: "$line479
-$line482 = $TAX_DEDUCTED_line437 + $line479
+line482 = `echo $TAX_DEDUCTED_line437 + $line479 | bc `
 echo "Line 482: "$line482
-IF $line435 > $line482 $line485 = $line435 - $line482
-IF $line435 < $line482 $line484 = $line435 - $line482
+
+if [ "$line435" -gt "$line482" ]
+then
+    line485 = `echo $line435 - $line482 | bc `
+fi
+
+if [ "$line435" -lt "$line482" ]
+then
+    line484 = `echo $line435 - $line482 | bc `
+fi
+
 echo "Line 484: "$line484
 echo "Line 485: "$line485
